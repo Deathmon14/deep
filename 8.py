47b134a4e5f74f95b94809dc14c3f728a2b607b4
Deathmon14 <150419471+Deathmon14@users.noreply.github.com>
2025-01-04 02:24:58 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -7,23 +7,20 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 import matplotlib.pyplot as plt
 
 # Load and preprocess text
-with open('your_text_file.txt', 'r') as file:
-    text = file.read().lower()
+text = open('your_text_file.txt', 'r').read().lower()
 
-# Tokenize text
+# Tokenize and generate sequences
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts([text])
 total_words = len(tokenizer.word_index) + 1
+seqs = [seq[:i+1] for line in text.split('\n') for seq in [tokenizer.texts_to_sequences([line])[0]] for i in range(1, len(seq))]
+max_len = max(len(seq) for seq in seqs)
+X = pad_sequences(seqs, maxlen=max_len, padding='pre')[:, :-1]
+y = tf.keras.utils.to_categorical([seq[-1] for seq in seqs], total_words)
 
-# Generate input sequences
-input_sequences = [seq[:i+1] for line in text.split('\n') for seq in [tokenizer.texts_to_sequences([line])[0]] for i in range(1, len(seq))]
-max_seq_len = max(len(seq) for seq in input_sequences)
-X = pad_sequences(input_sequences, maxlen=max_seq_len, padding='pre')[:, :-1]
-y = tf.keras.utils.to_categorical([seq[-1] for seq in input_sequences], total_words)
-
-# Build LSTM model
+# Build and train model
 model = Sequential([
-    Embedding(total_words, 100, input_length=max_seq_len - 1),
+    Embedding(total_words, 100, input_length=max_len - 1),
     LSTM(150, return_sequences=True),
     Dropout(0.2),
     LSTM(150),
@@ -31,25 +28,17 @@ model = Sequential([
     Dense(total_words, activation='softmax')
 ])
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# Train model
 history = model.fit(X, y, epochs=100, verbose=1)
 
-# Text generation function
-def generate_text(seed_text, next_words, model, max_seq_len):
-    for _ in range(next_words):
-        token_list = pad_sequences([tokenizer.texts_to_sequences([seed_text])[0]], maxlen=max_seq_len - 1, padding='pre')
-        seed_text += " " + tokenizer.index_word[np.argmax(model.predict(token_list, verbose=0))]
-    return seed_text
+# Generate text
+def gen_text(seed, words, model, max_len):
+    for _ in range(words):
+        seed += " " + tokenizer.index_word[np.argmax(model.predict(pad_sequences([tokenizer.texts_to_sequences([seed])[0]], maxlen=max_len - 1, padding='pre'), verbose=0))]
+    return seed
 
-# Generate example text
-print(generate_text("your seed text", 10, model, max_seq_len))
-
-# Plot training accuracy and loss
+# Generate and plot
+print(gen_text("your seed text", 10, model, max_len))
 plt.plot(history.history['accuracy'], label='Accuracy')
 plt.plot(history.history['loss'], label='Loss')
-plt.title('Model Training Progress')
-plt.xlabel('Epochs')
-plt.ylabel('Value')
 plt.legend()
 plt.show()
